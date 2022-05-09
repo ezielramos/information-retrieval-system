3b2f5fde969eb2cf1f150ad47e4de5c8e9b08972
ariel.plasencia <ariel.plasencia@gmail.com>
2022-05-09 01:01:45 -0400

    update main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from tools.tools import *
+from tools.similarity import *
+from evaluation_system.precision_measurements import *
 
 def documents_analyser(path):
 
@@ -14,6 +16,16 @@ def queries_analyser(path, a=0.5):
 
     return weight
 
+def answers_analyser(path, rank):
+
+    rr, ri, nr = get_rr_ri_nr(path, rank)
+
+    precision = PrecisionMeasurements.GetPrecision(rr, ri)
+    recovered = PrecisionMeasurements.GetRecovered(rr, nr)
+    measure_f1 = PrecisionMeasurements.GetMearureF(precision, recovered, beta=1)
+
+    return precision, recovered, measure_f1
+
 def main():
 
     queries_path = 'collections/cran/cran.qry'
@@ -26,5 +38,18 @@ def main():
     queries_weight = queries_analyser(queries_path)
     print(f'queries_weight = {queries_weight}\n')
     
+    rank = get_rank(queries_weight, documents_weight)
+    print(f'rank = {rank}\n')
+    
+    print_rank(rank)
+
+    precision, recovered, measure_f1 = answers_analyser(answers_path, rank)
+
+    print(f'precision = {precision}\n')
+    print(f'recobrado = {recovered}\n')
+    print(f'medida f1 = {measure_f1}\n')
+
+    print('DONE!!!')
+
 if __name__ == '__main__':
     main()
